8281a84172519251ad5d2f0c38a04a4b2ecaa968
mark-lutabi <mark.lutabi@mths.ca>
2024-10-29 10:47:15 -0400

    README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark Lutabi
+ * Created on: Oct, 2024
+ * This program counts down from 4-0 showing the appropiate neopixels.
 */
 
-basic.showString('Hello, World!')
